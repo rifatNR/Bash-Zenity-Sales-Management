0fb89e8626fbc7dac252b60a5258a229cea405d2
rifatNR <Rifatnoor92@gmail.com>
2022-04-22 21:23:08 +0000

    Changes

--- Test/zenity-test01.sh
+++ Test/zenity-test01.sh
@@ -8,3 +8,18 @@ do
   echo "$line"
   echo "-----"
 done < "$input"
+
+
+echo "||||||||||||||||||||||||||||||||||||||||||||||"
+
+while IFS=',' read -ra array; do
+  ar1+=("${array[0]}")
+  ar2+=("${array[1]}")
+  ar3+=("${array[2]}")
+  ar4+=("${array[3]}")
+done < "$input"                                                     
+
+
+# printf '%s\n' "${ar1[@]}" "${ar2[@]}" "${ar3[@]}" "${ar4[@]}"
+
+printf '%s\n' "${ar1[@]}"
