5a9336f7216ef00ea3b4945f440298d05c23da48
rifatNR <Rifatnoor92@gmail.com>
2022-04-28 03:24:06 +0600

    changes

--- Raw Project/update_form.sh
+++ Raw Project/update_form.sh
@@ -4,30 +4,32 @@ data=$(
 	zenity --forms --title="Update Product" --text="Update a product" --add-entry="Name" --add-entry="Price" --add-entry="Stock"
 )
 
+ret=$?
+echo $ret
 
 IFS="|" read -r name price stock <<< "$data" ;
-
-echo $?
 
 echo $name
 echo $price
 echo $stock
 
-case $? in
-    0)
-    	if [ "$name" == "" ] | [ "$price" == "" ] | [ "$stock" == "" ]
-    	then
-    		zenity --error --text="Please fill the required field!" --width=250
-    		bash update_form.sh $1
-    	else
-			bash update.sh $1 $name $price $stock
-			echo "Product added."
-			zenity --notification --window-icon="info" --text="Product Updated"
-			bash product_list.sh
-    	fi
-    	;;
-    1)
-	echo "Product not updated.";;
-    -1)
-	echo "An unexpected error has occurred.";;
-esac
+if [[ $ret == 1 ]]
+then
+	echo "No product updated."
+	bash options.sh
+	exit 99;
+fi
+
+if [[ $ret == 0 ]]
+then
+	if [ "$name" == "" ] | [ "$price" == "" ] | [ "$stock" == "" ]
+	then
+		zenity --error --text="Please fill the required field!" --width=250
+		bash update_form.sh $1
+	else
+		bash update.sh $1 $name $price $stock
+		echo "Product added."
+		zenity --notification --window-icon="info" --text="Product Updated"
+		bash product_list.sh
+	fi
+fi
